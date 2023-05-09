032639b26c9aaa18c34668f6cab1ea0116928ec6
Manuel Lara Torres <mlt00024@red.ujaen.es>
2023-05-09 19:24:20 +0200

    specs api-gateway de futbol añadidos

--- api-gateway/spec/server-spec.js
+++ api-gateway/spec/server-spec.js
@@ -233,6 +233,102 @@ describe('API Gateway: rutas estáticas', () => {
 
         })
     });
+    describe('Rutas estáticas de MS Plantilla', () => {
+        it('Devuelve MS Plantilla Home Page', (done) => {
+          supertest(app)
+            .get('/futbol/')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+              //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+              assert(res.body.hasOwnProperty('mensaje'));
+              assert(res.body.mensaje === "Microservicio MS Plantilla: home");
+    
+            })
+            .end((error) => { error ? done.fail(error) : done() })
+        });
+        it('Devuelve MS Plantilla Acerca De', (done) => {
+          supertest(app)
+            .get('/futbol/acercade')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+              //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
+              assert(res.body.hasOwnProperty('mensaje'));
+              assert(res.body.mensaje === "Microservicio MS Plantilla: acerca de");
+    
+            })
+            .end((error) => { error ? done.fail(error) : done() })
+        });
+      })
+      /**
+       * Tests para acceso a la BBDD
+       */
+      describe('Acceso a BBDD:', () => {
+        it('Devuelve los nombres al ir por la ruta test_db', (done) => {
+          supertest(app)
+            .get('/futbol/test_db')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+              //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+              assert(res.body.data[0].data.hasOwnProperty('nombre'));
+              assert(res.body.data[0].data.nombre === "Leo");
+    
+            })
+            .end((error) => { error ? done.fail(error) : done(); }
+            );
+        });
+    
+      })
+    
+      /**
+       * Listar jugadores
+       */
+      describe('Lista de jugadores:', () => {
+        it('Devuelve un listado con todos los jugadores', (done) => {
+          supertest(app)
+            .get('/futbol/get_jugadores')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+              //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+              assert(res.body.data[0].data.hasOwnProperty('nombre'));
+              assert(res.body.data[0].data.hasOwnProperty('apellidos'));
+              assert(res.body.data[0].data.nombre === "Leo");
+              assert(res.body.data[0].data.nombre != "Messi");
+              assert(res.body.data.length === 10);
+            })
+            .end((error) => { error ? done.fail(error) : done(); }
+            );
+        });
+      })
+      /**
+       * Listar todos los datos de los jugadores
+       */
+    describe('Lista de jugadores completa:', () => {
+      it('Devuelve un listado con todos los jugadores', (done) => {
+        supertest(app)
+          .get('/futbol/get_jugadores_completa')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function (res) {
+            //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+            assert(res.body.data[0].data.hasOwnProperty('nombre'));
+            assert(res.body.data[0].data.hasOwnProperty('apellidos'));
+            assert(res.body.data[0].data.hasOwnProperty('fnac'));
+            assert(res.body.data[0].data.hasOwnProperty('equipos'));
+            assert(res.body.data[0].data.hasOwnProperty('goles'));
+            assert(res.body.data[0].data.nombre === "Leo");
+            assert(res.body.data[0].data.nombre != "Messi");
+            assert(res.body.data[0].data.fnac.dia === 21);
+            assert(res.body.data[0].data.equipos[0] === "F.C Barcelona");
+            assert(res.body.data.length === 10);
+          })
+          .end((error) => { error ? done.fail(error) : done(); }
+          );
+      });
+    })
 
 
 
