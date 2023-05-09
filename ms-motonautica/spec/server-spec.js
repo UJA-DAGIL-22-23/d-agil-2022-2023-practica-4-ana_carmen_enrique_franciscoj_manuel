f0d8780f6e6bf3ecf67f8652556a57de2c6670d2
alp00054 <alp00054@red.ujaen.es>
2023-05-09 13:07:27 +0200

    carpeta ms-motonautica añadida

---
+++ ms-motonautica/spec/server-spec.js
@@ -0,0 +1,193 @@
+/**
+ * @file server-spec.js
+ * @description Fichero con la especificación de las pruebas TDD para server.js del MS MS motonautica
+ *              Este fichero DEBE llamarse server-spec.js
+ *              Este fichero DEBE ubicarse en el subdirectorio spec/
+ * @author Víctor M. Rivas Santos <vrivas@ujaen.es>
+ * @date 03-Feb-2023
+ */
+
+
+const supertest = require('supertest');
+const assert = require('assert')
+const app = require('../server');
+
+/**
+ * Test para las rutas "estáticas": / y /acerdade
+ */
+describe('Servidor MOTONAUTICA:', () => {
+  describe('Rutas / y /acercade', () => {
+    it('Devuelve MS motonautica Home Page', (done) => {
+      supertest(app)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS motonautica: home");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+    it('Devuelve MS motonautica Acerca De', (done) => {
+      supertest(app)
+        .get('/acercade')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS motonautica: acerca de");
+          assert(res.body.autor == "Ana León Pulido");
+          assert(res.body.email == "alp00054@red.ujaen.es");
+          assert(res.body.fecha == "18-04-2023");
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+  })
+
+  /**
+   * Tests para acceso a la BBDD
+   */
+  describe('Acceso a BBDD:', () => {
+    it('Devuelve nombre al consultar mediante test_db', (done) => {
+      supertest(app)
+        .get('/test_db')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.nombre === "Ana");
+
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Listar pilotos
+   */
+  describe('Lista de pilotos:', () => {
+    it('Devuelve un listado con todos los pilotos', (done) => {
+      supertest(app)
+        .get('/get_pilotos')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.hasOwnProperty('apellido'));
+          assert(res.body.data[0].data.nombre === "Ana");
+          assert(res.body.data[0].data.nombre != "Pato");
+          assert(res.body.data.length === 10);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Listar pilotos completo
+   */
+  describe('Lista de pilotos completa:', () => {
+    it('Devuelve un listado con todos los pilotos', (done) => {
+      supertest(app)
+        .get('/get_pilotos_completos')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.hasOwnProperty('apellido'));
+          assert(res.body.data[0].data.hasOwnProperty('edad'));
+          assert(res.body.data[0].data.hasOwnProperty('motos'));
+          assert(res.body.data[0].data.hasOwnProperty('playasvisitadas'));
+          assert(res.body.data[0].data.nombre === "Ana");
+          assert(res.body.data[0].data.nombre != "Pato");
+          assert(res.body.data.length === 10);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Tests para acceso a la BBDD
+   */
+  describe('Acceso a BBDD:', () => {
+    it('Devuelve nombre al consultar mediante test_db', (done) => {
+      supertest(app)
+        .get('/test_db')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.nombre === "Ana");
+
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Listar pilotos
+   */
+  describe('Lista de pilotos:', () => {
+    it('Devuelve un listado con todos los pilotos', (done) => {
+      supertest(app)
+        .get('/get_pilotos')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.hasOwnProperty('apellido'));
+          assert(res.body.data[0].data.nombre === "Ana");
+          assert(res.body.data[0].data.nombre != "Pato");
+          assert(res.body.data.length === 10);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Listar pilotos completo
+   */
+  describe('Lista de pilotos completa:', () => {
+    it('Devuelve un listado con todos los pilotos', (done) => {
+      supertest(app)
+        .get('/get_pilotos_completos')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.hasOwnProperty('apellido'));
+          assert(res.body.data[0].data.hasOwnProperty('edad'));
+          assert(res.body.data[0].data.hasOwnProperty('motos'));
+          assert(res.body.data[0].data.hasOwnProperty('playasvisitadas'));
+          assert(res.body.data[0].data.nombre === "Ana");
+          assert(res.body.data[0].data.nombre != "Pato");
+          assert(res.body.data.length === 10);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+});
+
+
+
