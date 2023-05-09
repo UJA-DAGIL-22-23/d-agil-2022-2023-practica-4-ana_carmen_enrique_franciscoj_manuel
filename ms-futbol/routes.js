a9fa93ca6a0553adae829e191b8a16623ac78976
Manuel Lara Torres <mlt00024@red.ujaen.es>
2023-05-09 12:17:09 +0200

    Carpeta ms-futbol añadida

---
+++ ms-futbol/routes.js
@@ -0,0 +1,72 @@
+/**
+ * @file routes.js
+ * @description Define las rutas ante las que va a responder al MS Futbol
+ * @author Víctor M. Rivas <vrivas@ujaen.es>
+ * @date 03-feb-2023
+ */
+
+const express = require("express");
+const router = express.Router();
+const { callbacks } = require("./callbacks");
+
+
+
+/**
+ * Ruta raíz: /
+ */
+router.get("/", async (req, res) => {
+    try {
+        await callbacks.home(req, res)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * Ruta Acerca De (es decir, About...)
+ */
+router.get("/acercade", async (req, res) => {
+    try {
+        await callbacks.acercaDe(req, res)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+/**
+ * Test de conexión a la BBDD
+ */
+router.get("/test_db", async (req, res) => {
+    try {
+        await callbacks.test_db(req, res)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * Ruta de get_jugadores
+ */
+router.get("/get_jugadores", async (req, res) => {
+    try {
+        await callbacks.get_jugadores(req, res)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * Ruta de get_jugadores completa
+ */
+router.get("/get_jugadores_completa", async (req, res) => {
+    try {
+        await callbacks.get_jugadores_completa(req, res)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Exporto el módulo para poder usarlo en server
+module.exports = router;
---
+++ ms-futbol/spec/server-spec.js
@@ -0,0 +1,121 @@
+/**
+ * @file server-spec.js
+ * @description Fichero con la especificación de las pruebas TDD para server.js del MS MS Futbol
+ *              Este fichero DEBE llamarse server-spec.js
+ *              Este fichero DEBE ubicarse en el subdirectorio spec/
+ * @author Víctor M. Rivas Santos <vrivas@ujaen.es>
+ * @date 03-Feb-2023
+ */
+
+
+const supertest = require('supertest');
+const assert = require('assert')
+const app = require('../server');
+
+/**
+ * Test para las rutas "estáticas": / y /acerdade
+ */
+describe('Servidor FUTBOL:', () => {
+  describe('Rutas / y /acercade', () => {
+    it('Devuelve MS Futbol Home Page', (done) => {
+      supertest(app)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Futbol: home");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+    it('Devuelve MS Futbol Acerca De', (done) => {
+      supertest(app)
+        .get('/acercade')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Futbol: acerca de");
+          assert(res.body.autor == "Manuel Lara Torres");
+          assert(res.body.email == "mlt00024@red.ujaen.es");
+          assert(res.body.fecha == "21-04-2023");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+  })
+
+  /**
+   * Tests para acceso a la BBDD
+   */
+  describe('Acceso a BBDD:', () => {
+    it('Devuelve los nombres al ir por la ruta test_db', (done) => {
+      supertest(app)
+        .get('/test_db')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.nombre === "Leo");
+
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+   * Listar jugadores
+   */
+  describe('Lista de jugadores:', () => {
+    it('Devuelve un listado con todos los jugadores', (done) => {
+      supertest(app)
+        .get('/get_jugadores')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.hasOwnProperty('apellidos'));
+          assert(res.body.data[0].data.nombre === "Leo");
+          assert(res.body.data[0].data.nombre != "Messi");
+          assert(res.body.data.length === 10);
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+  })
+  /**
+   * Listar todos los datos de los jugadores
+   */
+describe('Lista de jugadores completa:', () => {
+  it('Devuelve un listado con todos los jugadores', (done) => {
+    supertest(app)
+      .get('/get_jugadores_completa')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function (res) {
+        //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+        assert(res.body.data[0].data.hasOwnProperty('nombre'));
+        assert(res.body.data[0].data.hasOwnProperty('apellidos'));
+        assert(res.body.data[0].data.hasOwnProperty('fnac'));
+        assert(res.body.data[0].data.hasOwnProperty('equipos'));
+        assert(res.body.data[0].data.hasOwnProperty('goles'));
+        assert(res.body.data[0].data.nombre === "Leo");
+        assert(res.body.data[0].data.nombre != "Messi");
+        assert(res.body.data[0].data.fnac.dia === 21);
+        assert(res.body.data[0].data.equipos[0] === "F.C Barcelona");
+        assert(res.body.data.length === 10);
+      })
+      .end((error) => { error ? done.fail(error) : done(); }
+      );
+  });
+})
+});
+
+
