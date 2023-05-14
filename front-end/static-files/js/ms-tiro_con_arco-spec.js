e43b8f0e7e3952577d0570162f8cc24dcfd01b15
chg00016 <124682754+chg00016@users.noreply.github.com>
2023-05-14 17:54:21 +0200

    Corrigiendo error en ms-tiro_con_arco-spec.js

--- front-end/static-files/js/ms-tiro_con_arco-spec.js
+++ front-end/static-files/js/ms-tiro_con_arco-spec.js
@@ -8,7 +8,7 @@
 // SPECS para Jasmine
 
 // Constantes para usar en las pruebas
-const elementoTitulo = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_TITULO)
+const elementoTiroConArco = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_TITULO)
 const elementoContenido = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_CONTENIDO)
 const TITULO_HOME = "tiro_con_arco Home"
 const TITULO_ACERCA_DE = "tiro_con_arco Acerca de"
@@ -39,14 +39,14 @@ describe("tiro_con_arco.mostrarHome: ", function () {
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             tiro_con_arco.mostrarHome()
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_HOME)
             expect(elementoContenido.innerHTML).toBe(tiro_con_arco.datosDescargadosNulos.mensaje)
         })
 
     it("muestra datos nulos cuando le pasamos un valor que no es un objeto",
         function () {
             tiro_con_arco.mostrarHome(23)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_HOME)
             expect(elementoContenido.innerHTML).toBe(tiro_con_arco.datosDescargadosNulos.mensaje)
         })
 
@@ -54,19 +54,19 @@ describe("tiro_con_arco.mostrarHome: ", function () {
         function () {
             // Objeto vacío
             tiro_con_arco.mostrarHome({})
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_HOME)
             expect(elementoContenido.innerHTML).toBe(tiro_con_arco.datosDescargadosNulos.mensaje)
 
             // Objeto sin campo mensaje
             tiro_con_arco.mostrarHome({ foo: "bar" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_HOME)
             expect(elementoContenido.innerHTML).toBe(tiro_con_arco.datosDescargadosNulos.mensaje)
         })
 
     it("muestra correctamente el título y el mensaje",
         function () {
             tiro_con_arco.mostrarHome(datosDescargadosPrueba)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_HOME)
             expect(elementoContenido.innerHTML).toBe(datosDescargadosPrueba.mensaje)
         })
 })
@@ -76,14 +76,14 @@ describe("tiro_con_arco.mostrarAcercaDe: ", function () {
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             tiro_con_arco.mostrarAcercaDe()
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
         })
 
     it("muestra datos nulos cuando le pasamos un valor que no es un objeto",
         function () {
             tiro_con_arco.mostrarAcercaDe(23)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
         })
 
@@ -91,30 +91,30 @@ describe("tiro_con_arco.mostrarAcercaDe: ", function () {
         function () {
             // Objeto vacío
             tiro_con_arco.mostrarAcercaDe({})
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
 
             // Objeto sin campo mensaje
             tiro_con_arco.mostrarAcercaDe({ autor: "un autor", email: "un email", fecha: "una fecha" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
             // Objeto sin campo autor
             tiro_con_arco.mostrarAcercaDe({ mensaje: "un mensaje", email: "un email", fecha: "una fecha" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
             // Objeto sin campo email
             tiro_con_arco.mostrarAcercaDe({ mensaje: "un mensaje", autor: "un autor", fecha: "una fecha" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
             // Objeto sin campo fecha
             tiro_con_arco.mostrarAcercaDe({ mensaje: "un mensaje", autor: "un autor", email: "un email" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
             expect(elementoContenido.innerHTML.search(tiro_con_arco.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
         })
     it("muestra correctamente el título y el mensaje conteniendo el autor, el email y la fecha",
         function () {
             tiro_con_arco.mostrarAcercaDe(datosDescargadosPrueba)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            expect(elementoTiroConArco.innerHTML).toBe(TITULO_ACERCA_DE)
 
             // Comprobamos que al buscar el autor, el email y la fecha de prueba los encuentra dentro del contenido del article
             expect(elementoContenido.innerHTML.search(datosDescargadosPrueba.autor) >= 0).toBeTrue()
@@ -128,7 +128,7 @@ describe("tiro_con_arco.imprimeOrdenados: ", function() {
     it("Mostrar datos nulos cuando le pasamos vector nulo", 
         function() {
             tiro_con_arco.imprimeOrdenados([])
-            expect(elementoTitulo.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros ordenados")
+            expect(elementoTiroConArco.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros ordenados")
             expect(elementoContenido.querySelector('tbody').innerHTML).toBe('')
     })
 })
@@ -137,7 +137,7 @@ describe("tiro_con_arco.imprimeCompleto: ", function() {
     it("Mostrar datos nulos cuando le pasamos un valor que no es un objeto",
     function() {
         tiro_con_arco.imprimeCompleto(10)
-        expect(elementoTitulo.innerHTML).toBe("tiro_con_arco del listado de todos los datos de todos los arqueros")
+        expect(elementoTiroConArco.innerHTML).toBe("tiro_con_arco del listado de todos los datos de todos los arqueros")
         expect(elementoContenido.querySelector('tbody').innerHTML).toBe('')
 })
 })
@@ -147,13 +147,13 @@ describe("tiro_con_arco.imprimeSoloNombres: ", function() {
     it("Mostrar datos nulos cuando le pasamos un valor que no es un objeto",
     function() {
         tiro_con_arco.imprimeSoloNombres(10)
-        expect(elementoTitulo.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros")
+        expect(elementoTiroConArco.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros")
         expect(elementoContenido.querySelector('tbody').innerHTML).toBe('')
 })
     it("Mostrar datos nulos cuando le pasamos vector nulo", 
     function() {
     tiro_con_arco.imprimeSoloNombres([])
-    expect(elementoTitulo.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros")
+    expect(elementoTiroConArco.innerHTML).toBe("tiro_con_arco del listado de los nombres de todos los arqueros")
     expect(elementoContenido.querySelector('tbody').innerHTML).toBe('')
 })
 })
